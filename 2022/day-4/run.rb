5f2e30fd16a9dfbcdf770700be85557e98f78964
leandronsp <leandronsp@gmail.com>
2022-12-17 17:11:21 -0300

    day 4, part I

---
+++ 2022/day-4/run.rb
@@ -0,0 +1,49 @@
+require 'test/unit'
+
+class Day4Test < Test::Unit::TestCase
+  def build_range(range)
+    range
+      .split('-')
+      .values_at(0, 1)
+      .then { |(from, to)| (from.to_i..to.to_i) }
+      .then(&:to_a)
+  end
+
+  def fully_contains?(left, right)
+    intersection = left & right
+    intersection == left || intersection == right
+  end
+
+  def test_build_range
+    assert_equal [2, 3, 4], build_range('2-4')
+  end
+
+  def test_fully_contains
+    assert fully_contains?([3, 4], [1, 2, 3, 4])
+    assert fully_contains?([1, 2, 3, 4], [3, 4])
+
+    refute fully_contains?([3, 4], [4, 5, 6])
+    refute fully_contains?([4, 5, 6], [3, 4])
+  end
+
+  def pairs_containing_each_other(input)
+    input
+      .split("\n")
+      .map { |row| row.split(",") }
+      .map { |(left, right)| [build_range(left), build_range(right)] }
+      .select { |(left, right)| fully_contains?(left, right) }
+      .size
+  end
+
+  def test_simple
+    input = File.read('2022/day-4/input-test')
+
+    assert_equal 2, pairs_containing_each_other(input)
+  end
+
+  def test_complex
+    input = File.read('2022/day-4/input')
+
+    assert_equal 569, pairs_containing_each_other(input)
+  end
+end
